062e112d0fc96f61a828ef53abd30fa999f87850
Tyfenn Forner <tyfenn.forner@hotmail.com>
2024-05-29 13:43:34 +0200

    Initial Commit

---
+++ sitefinal/sitefinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  sitefinalApp.swift
+//  sitefinal
+//
+//  Created by Tyfenn Forner on 29/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct sitefinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
